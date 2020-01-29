93a33dac4c05ccedceac5ad38a871dc5e0a79361
James Schumann <james@schumann.ca>
2020-01-29 14:09:35 -0500

    add memberships-all-staff-calculate

---
+++ memberships-all-staff-calculate/index.ts
@@ -0,0 +1,68 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { createLogObject } from "../SharedCode/createLogObject";
+import { createLogBlob } from "../SharedCode/createLogBlob";
+import { createCallbackMessage } from "../SharedCode/createCallbackMessage";
+import { createEvent } from "../SharedCode/createEvent";
+import { createBlob } from "../SharedCode/createBlob";
+
+const membershipsAllStaffCalculate: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.SortingHat.Memberships.AllStaff.Calculate';
+    const functionEventID = `sorting-hat-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-memberships-all-staff-calculate-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "sorting-hat", 
+    ];
+
+    const blobStorageAccount = process.env['storageAccount'];
+    const blobStorageKey = process.env['storageKey'];
+    const blobStorageContainer = 'set-memberships-now';
+    const blobName = 'all-staff.json';
+
+    const rows = context.bindings.iamwpRaw;
+
+    const excluded_job_codes = ['6106', '6118'];
+    const activity_codes = ['ACTIVE', 'ONLEAVE'];
+
+    let members = [];
+
+    rows.forEach(function(row) {
+        if (row.EMAIL_ADDRESS 
+            && !excluded_job_codes.includes(row.JOB_CODE)
+            && activity_codes.includes(row.ACTIVITY_CODE)
+        ) {
+            members.push(row.EMAIL_ADDRESS);
+        }
+    });
+
+    let memberships = JSON.stringify(members);
+
+    let result = await createBlob(blobStorageAccount, blobStorageKey, blobStorageContainer, blobName, memberships);
+
+    const logPayload = result;
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await createLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(functionInvocationID, functionInvocationTime, functionInvocationTimestamp, functionName, functionEventType, functionEventID, functionLogID, logStorageAccount, logStorageContainer, eventLabel, eventTags);
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default membershipsAllStaffCalculate;
