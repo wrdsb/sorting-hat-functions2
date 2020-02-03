38c816fe5a1147c1dd1eef8ee626066fe7b0d056
James Schumann <james@schumann.ca>
2020-02-03 13:50:53 -0500

    ipps-locations-reconcile working

--- ipps-locations-reconcile/index.ts
+++ ipps-locations-reconcile/index.ts
@@ -1,10 +1,17 @@
 import { AzureFunction, Context } from "@azure/functions"
 import { CosmosClient } from "@azure/cosmos";
-import { isEqual } from "lodash";
 import { createLogObject } from "../shared/createLogObject";
 import { createLogBlob } from "../shared/createLogBlob";
 import { createCallbackMessage } from "../shared/createCallbackMessage";
 import { createEvent } from "../shared/createEvent";
+import { initCalculation } from "../shared/initCalculation";
+import { initOperations } from "../shared/initOperations";
+import { findCreatesAndUpdates } from "../shared/findCreatesAndUpdates";
+import { findDeletes } from "../shared/findDeletes";
+import { processCreates } from "../shared/processCreates";
+import { processUpdates } from "../shared/processUpdates";
+import { processDeletes } from "../shared/processDeletes";
+import { getCosmosItems } from "../shared/getCosmosItems";
 
 const ippsLocationsReconcile: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
     const functionInvocationID = context.executionContext.invocationId;
@@ -28,45 +35,46 @@ const ippsLocationsReconcile: AzureFunction = async function (context: Context, 
     const cosmosEndpoint = process.env['cosmosEndpoint'];
     const cosmosKey = process.env['cosmosKey'];
     const cosmosDatabase = process.env['cosmosDatabase'];
-    const cosmosContainer = 'people-sets';
+    const cosmosContainer = 'people-set-definitions';
+    const cosmosQuery = "SELECT * FROM c WHERE c.type = 'ipps-location'";
     const cosmosClient = new CosmosClient({endpoint: cosmosEndpoint, auth: {masterKey: cosmosKey}});
 
     // give our bindings more human-readable names
     const locations_now = context.bindings.locationsNow;
+    context.log('Found ' + Object.getOwnPropertyNames(locations_now).length + ' locations in IPPS.')
+
+    // fetch previous records from Cosmos
+    let records_previous = await getCosmosItems(context, cosmosClient, cosmosDatabase, cosmosContainer, cosmosQuery).catch(err => {
+        context.log(err);
+    });
+    context.log('Found ' + Object.getOwnPropertyNames(records_previous).length + ' IPPS locations in the Sorting Hat.')
+
 
     let records_now = await materializeLocations(locations_now);
 
-    // fetch current records from Cosmos
-    const records_previous = await getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer).catch(err => {
-        context.log(err);
-    });
+    let calculation = await initCalculation(
+        context,
+        records_previous,
+        records_now
+    );
+    calculation = await findCreatesAndUpdates(context, calculation);
+    calculation = await findDeletes(context, calculation);
 
-    // object to store our total diff as we build it
-    let calculation = {
-        records_previous: records_previous,
-        records_now: records_now,
-        differences: {
-            created_records: [],
-            updated_records: [],
-            deleted_records: []
-        }
+    let operations = await initOperations(context);
+    operations.creates = await processCreates(context, calculation.differences.created_records);
+    operations.updates = await processUpdates(context, calculation.differences.updated_records);
+    operations.deletes = await processDeletes(context, calculation.differences.deleted_records);
+
+    let queueMessage = operations.creates.concat(
+        operations.updates,
+        operations.deletes
+    );
+    context.bindings.queuePeopleSetDefinitionStore = JSON.stringify(queueMessage);
+
+    const logPayload = {
+        calculation: calculation,
+        operations: operations
     };
-
-    calculation = await findCreatesAndUpdates(calculation);
-    calculation = await findDeletes(calculation);
-
-    let creates = await processCreates(calculation.differences.created_records);
-    let updates = await processUpdates(calculation.differences.updated_records);
-    let deletes = await processDeletes(calculation.differences.deleted_records);
-
-    let differences = await processDifferences(calculation.differences);
-    let totalDifferences = await calculateTotalDifferences(calculation);
-
-    if (totalDifferences > 0) {
-        context.bindings.queuePeopleSetStore = creates.concat(updates, deletes);
-    }
-
-    const logPayload = calculation;
     const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
     const logBlob = await createLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
     context.log(logBlob);
@@ -83,6 +91,9 @@ const ippsLocationsReconcile: AzureFunction = async function (context: Context, 
 
     async function materializeLocations(locations)
     {
+        context.log('Materialize locations...');
+        context.log('Materializing ' + Object.getOwnPropertyNames(locations).length + ' locations...')
+        
         let materializedLocations = {};
 
         Object.getOwnPropertyNames(locations).forEach(function (location_id) {
@@ -91,219 +102,19 @@ const ippsLocationsReconcile: AzureFunction = async function (context: Context, 
             let materializedLocation = {
                 id:         `LC-${locationRecord.id}`,
                 atomic:     true,
+                type:       'ipps-location',
                 name:       locationRecord.location_description,
+                short_name: locationRecord.location_description,
                 aliases:    [],
                 categories: ["IPPS", "ipps-locations", "location-codes"],
                 tags:       []
             };
 
-            materializedLocation[materializedLocation.id] = materializedLocation;
+            materializedLocations[materializedLocation.id] = materializedLocation;
         });
-        
+
+        context.log('Materialized ' + Object.getOwnPropertyNames(materializedLocations).length + ' locations.')
         return materializedLocations;
-    }
-
-    async function findCreatesAndUpdates(calculation)
-    {
-        context.log('findCreatesAndUpdates');
-
-        let records_previous = calculation.records_previous;
-        let records_now = calculation.records_now;
-
-        // loop through all records in records_now, looking for updates and creates
-        Object.getOwnPropertyNames(records_now).forEach(function (record_id) {
-            let new_record = records_now[record_id];      // get the full person record from records_now
-            let old_record = records_previous[record_id]; // get the corresponding record in records_previous
-    
-            // if we found a corresponding record in records_previous, look for changes
-            if (old_record) {
-                // Compare old and new records using Lodash _.isEqual, which performs a deep comparison
-                let records_equal = isEqual(old_record, new_record);
-    
-                // if record changed, record the change
-                if (!records_equal) {
-                    calculation.differences.updated_records.push({
-                        previous: old_record,
-                        now: new_record
-                    });
-                }
-   
-            // if we don't find a corresponding record in records_previous, they're new
-            } else {
-                calculation.differences.created_records.push(new_record);
-            }
-        });
-
-        return calculation;
-    }
-
-    async function findDeletes(calculation)
-    {
-        context.log('findDeletes');
-
-        let records_previous = calculation.records_previous;
-        let records_now = calculation.records_now;
-
-        // loop through all records in records_previous, looking for deletes
-        Object.getOwnPropertyNames(records_previous).forEach(function (record_id) {
-            let new_record = records_now[record_id];
-    
-            if (!new_record) {
-                // the record was deleted
-                calculation.differences.deleted_records.push(records_previous[record_id]);
-            }
-        });
-
-        return calculation;
-    }
-
-    async function processCreates(created_records)
-    {
-        context.log('processCreates');
-
-        // array for the results being returned
-        let messages = [];
-
-        created_records.forEach(function (record) {
-            let message = {
-                operation: 'replace',
-                payload: record
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        return messages;
-    }
-
-    async function processUpdates(updated_records)
-    {
-        context.log('processUpdates');
-
-        // array for the results being returned
-        let messages = [];
-
-        updated_records.forEach(function (record) {
-            let message = {
-                operation: 'replace',
-                payload: record.now
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        return messages;
-    }
-
-    async function processDeletes(deleted_records)
-    {
-        context.log('processDeletes');
-
-        // array for the results being returned
-        let messages = [];
-
-        deleted_records.forEach(function (record) {
-            let message = {
-                operation: 'delete',
-                payload: record
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        return messages;
-    }
-
-    async function processDifferences(differences)
-    {
-        context.log('processDifferences');
-
-        // array for the results being returned
-        let messages = [];
-
-        differences.created_records.forEach(function (record) {
-            let message = {
-                operation: 'create',
-                payload: record
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        differences.updated_records.forEach(function (record) {
-            let message = {
-                operation: 'update',
-                payload: record
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        differences.deleted_records.forEach(function (record) {
-            let message = {
-                operation: 'delete',
-                payload: record
-            };
-            messages.push(JSON.stringify(message));
-        });
-
-        return messages;
-    }
-
-    async function calculateTotalDifferences (calculation)
-    {
-        let creates = calculation.differences.created_records.length;
-        let updates = calculation.differences.updated_records.length;
-        let deletes = calculation.differences.deleted_records.length;
-        let totalDifferences = creates + updates + deletes;
-
-        return totalDifferences;
-    }
-
-    async function getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer)
-    {
-        context.log('getCosmosItems');
-
-        let records_previous = {};
-
-        const querySpec = {
-            query: `SELECT * FROM c WHERE ARRAY_CONTAINS(c.categories, 'ipps-locations')`
-        }
-        const queryOptions  = {
-            maxItemCount: -1,
-            enableCrossPartitionQuery: true
-        }
-
-        const queryIterator = await cosmosClient.database(cosmosDatabase).container(cosmosContainer).items.query(querySpec, queryOptions);
-        
-        while (queryIterator.hasMoreResults()) {
-            const results = await queryIterator.executeNext();
-
-            records_previous = await consolidateCosmosItems(results.result, records_previous);
-
-            if (results === undefined) {
-                // no more results
-                break;
-            }   
-        }
-
-        return records_previous;
-    }
-
-    async function consolidateCosmosItems(items: any[], consolidatedObject)
-    {
-        items.forEach(function(item) {
-            // These fields are not present in the data from IPPS
-            // They are added by Flenderson when the person is created/updated/deleted
-            delete item.created_at;
-            delete item.updated_at;
-            delete item.deleted_at;
-            delete item.deleted;
-            delete item._rid;
-            delete item._self;
-            delete item._etag;
-            delete item._attachments;
-            delete item._ts;
-
-            consolidatedObject[item.id] = item;
-        });
-
-        return consolidatedObject;
     }
 };
 
