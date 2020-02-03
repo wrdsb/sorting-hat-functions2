0da6d800a6a2fd8fa66a3bb83546c67cb12823bc
James Schumann <james@schumann.ca>
2020-02-03 13:52:05 -0500

    standarize ping function

--- ping/index.ts
+++ ping/index.ts
@@ -1,21 +1,14 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+const ping: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('Sorting Hat ping: running');
 
-    if (name) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    const res = 'pong';
+
+    context.res = {
+        status: 200,
+        body: res
+    };
 };
 
-export default httpTrigger;
+export default ping;
