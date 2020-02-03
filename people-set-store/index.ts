a0bf5c99db23524ed8cd641264d9bf9a476bf393
James Schumann <james@schumann.ca>
2020-02-03 13:52:39 -0500

    people-set-store should be people-set-definition-store

--- people-set-store/index.ts
+++
@@ -1,7 +0,0 @@
-import { AzureFunction, Context } from "@azure/functions"
-
-const peopleSetStore: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
-    context.log('Queue trigger function processed work item', triggerMessage);
-};
-
-export default peopleSetStore;
