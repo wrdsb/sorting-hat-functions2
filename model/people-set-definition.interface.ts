b7ea3394143957ec825dbc00a1fe6fa28fb7b51a
James Schumann <james@schumann.ca>
2020-03-09 09:57:39 -0400

    move people-set interface to people-set-definition interface

---
+++ model/people-set-definition.interface.ts
@@ -0,0 +1,28 @@
+type PeopleSetType = string;
+type PeopleSetSet = string[];
+
+interface PeopleSetDefinition
+{
+    id: string;
+
+    atomic: boolean;
+
+    type: PeopleSetType;
+
+    name: string,
+    short_name: string;
+    aliases: string[];
+
+    categories: string[];
+    tags: string[];
+
+    definition: PeopleSetSet[];
+    constituent_sets: string[];
+
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+    deleted: boolean;
+}
+
+export { PeopleSetType, PeopleSetSet, PeopleSetDefinition };
