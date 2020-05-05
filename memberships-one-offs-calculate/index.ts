f84c2722be86cccd64e0e0525409c793ac5681b1
James Schumann <james@schumann.ca>
2020-05-05 15:24:32 -0400

    remove duplicate calculations for elementary and secondary serts

--- memberships-one-offs-calculate/index.ts
+++ memberships-one-offs-calculate/index.ts
@@ -29,9 +29,9 @@ const membershipsOneOffsCalculate: AzureFunction = async function (context: Cont
     const blobStorageContainer = 'set-memberships-now';
 
     const elementary_serts_job_codes = context.bindings.elementarySertsJobCodes.definition[0];
+    const secondary_serts_job_codes = context.bindings.secondarySertsJobCodes.definition[0];
     const itinerant_spec_ed_job_codes = context.bindings.itinerantSpecEdJobCodes.definition[0];
     const itinerant_spec_ed_location_codes = context.bindings.itinerantSpecEdLocationCodes.definition[0];
-    const secondary_serts_job_codes = context.bindings.secondarySertsJobCodes.definition[0];
     const smaca_elementary_group_codes = context.bindings.smacaElementaryGroupCodes.definition[0];
     const smaca_secondary_group_codes = context.bindings.smacaSecondaryGroupCodes.definition[0];
 
@@ -89,9 +89,9 @@ const membershipsOneOffsCalculate: AzureFunction = async function (context: Cont
     async function calculateMembers (rows) {
         let members = {};
         members['elementary-serts-job-codes'] = [];
+        members['secondary-serts-job-codes'] = [];
         members['intranet-trillium-job-codes'] = [];
         members['itinerant-spec-ed-job-codes'] = [];
-        members['secondary-serts-job-codes'] = [];
         members['smaca-elementary-group-codes'] = [];
         members['smaca-secondary-group-codes'] = [];
 
@@ -123,17 +123,11 @@ const membershipsOneOffsCalculate: AzureFunction = async function (context: Cont
                 if (elementary_serts_job_codes.includes(job_code) && panel == 'E') {
                     members['elementary-serts-job-codes'].push(person);
                 }
-                if (elementary_serts_job_codes.includes(job_code) && panel == 'E') {
-                    members['elementary-serts-job-codes'].push(person);
-                }
                 if (secondary_serts_job_codes.includes(job_code) && panel == 'S') {
                     members['secondary-serts-job-codes'].push(person);
                 }
                 if (itinerant_spec_ed_job_codes.includes(job_code) && itinerant_spec_ed_location_codes.includes(location_code)) {
                     members['itinerant-spec-ed-job-codes'].push(person);
-                }
-                if (secondary_serts_job_codes.includes(job_code) && panel == 'S') {
-                    members['secondary-serts-job-codes'].push(person);
                 }
                 if (smaca_elementary_group_codes.includes(group_code) && panel == 'E') {
                     members['smaca-elementary-group-codes'].push(person);
