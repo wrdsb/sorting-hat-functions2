f4e617ff6bc95ecea27f2c282655ff2632e1b330
James Schumann <james@schumann.ca>
2020-05-05 11:45:20 -0400

    add memberships-calculate-all-timer. closes #4

---
+++ memberships-calculate-all-timer/index.ts
@@ -0,0 +1,59 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { createLogObject } from "../shared/createLogObject";
+import { createLogBlob } from "../shared/createLogBlob";
+import { createCallbackMessage } from "../shared/createCallbackMessage";
+import { createEvent } from "../shared/createEvent";
+import { createBlob } from "../shared/createBlob";
+
+const membershipsCalculateAllTimer: AzureFunction = async function (context: Context, timer: any): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.SortingHat.Memberships.Calculate.Timer';
+    const functionEventID = `sorting-hat-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-memberships-calculate-all-timer-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "sorting-hat", 
+    ];
+
+    context.bindings.membershipsABCCalculateAll = "now";
+    context.bindings.membershipsAllStaffCalculate = "now";
+    context.bindings.membershipsOneOffsCalculate = "now";
+    context.bindings.membershipsRolesCalculate = "now";
+    context.bindings.membershipsStudentsCalculate = "now";
+    context.bindings.membershipsUnionsCalculate = "now";
+
+    const logPayload = {
+        invoke: [
+            "memberships-abc-calculate-all",
+            "memberships-all-staff-calculate",
+            "memberships-one-offs-calculate",
+            "memberships-roles-calculate",
+            "memberships-students-calculate",
+            "memberships-unions-calculate"
+        ]
+    };
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await createLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(functionInvocationID, functionInvocationTime, functionInvocationTimestamp, functionName, functionEventType, functionEventID, functionLogID, logStorageAccount, logStorageContainer, eventLabel, eventTags);
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default membershipsCalculateAllTimer;
